0416a604f2d91bf7ea1eead51202f5fa33a8f645
Zake <56502238+Zakede@users.noreply.github.com>
2021-06-05 17:13:06 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main(){
     println!("Simple Rust Calculator!!");
     let mut num1 = String::new();
     let mut num2 = String::new();
-    let mut operatorlol = String::new();
+    let mut operator = String::new();
 
     print!("Write The First Number: ");
     read(&mut num1);
@@ -21,18 +21,18 @@ fn main(){
     read(&mut num2);
 
     print!("Please Choose an Operator?\n[+, -, /, *]");
-    read(&mut operatorlol);
+    read(&mut operator);
     let num1:f64 = num1.trim().parse().unwrap();
     let num2:f64 = num2.trim().parse().unwrap();
-    let operatorlol:char = operatorlol.trim().chars().next().unwrap();
+    let operator:char = operator.trim().chars().next().unwrap();
     
     let operators = String::from("+-*/");
-    if !operators.contains(operatorlol) {
+    if !operators.contains(operator) {
         println!("Unkonw Operator");
         return;
     }
 
-    let result = match operatorlol {
+    let result = match operator {
         '+' => num1 + num2,
         '-' => num1 - num2,
         '*' => num1 * num2,
@@ -40,5 +40,5 @@ fn main(){
         _ => panic!("Incorrect Operator, Make Sure You Input Correct Operator!")
     };
 
-    println!("Final Result Of {} {} {} = {}", num1, operatorlol, num2, result);
+    println!("Final Result Of {} {} {} = {}", num1, operator, num2, result);
 }
